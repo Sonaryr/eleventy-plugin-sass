c6f9a4b99ccc003cbb96db1df5580f540159530c
Michoel Samuels <k2co3@icloud.com>
2022-02-23 03:06:52 +0200

    Add gulp-sass

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const sass = require('sass');
+const sass = require('gulp-sass')(require('sass'));
 const chokidar = require('chokidar');
 const vfs = require('vinyl-fs');
 const sourcemaps = require('gulp-sourcemaps');
